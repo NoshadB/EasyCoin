f8c3afb868b0976376bbbdf52d520e579eafec4a
Ayo <71637498+AyomideOladele@users.noreply.github.com>
2023-03-12 23:55:25 +0000

    initial commit

---
+++ src/main/java/com/example/easycoin/HelloApplication.java
@@ -0,0 +1,26 @@
+package com.example.easycoin;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        stage.setTitle("EasyCoin");
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
